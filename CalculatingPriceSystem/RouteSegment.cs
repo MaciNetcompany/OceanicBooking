c189ecfc9337001f425607fb687d8fb246672a6d
Katarzyna Kołodziej <katarzynakolodziej1998@wp.pl>
2023-01-12 16:03:54 +0100

    added routesegment class

---
+++ CalculatingPriceSystem/RouteSegment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingPriceSystem
+{
+    internal class RouteSegment
+    {
+
+        private String PointA;
+        private String PointB;
+        private String Vehicle;
+        public RouteSegment(String Point_A, String Point_B, String Veh)
+        {
+            PointA = Point_A;
+            PointB = Point_B;
+            Vehicle = Veh;
+        }
+    }
+}
