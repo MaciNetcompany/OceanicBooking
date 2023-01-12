443b6c86ed545bda3b826f46213537344f2dadcc
kbkolodziej <49360608+kbkolodziej@users.noreply.github.com>
2023-01-12 17:56:47 +0100

    Create RouteSegment.cs

---
+++ CalculatingPriceSystem/RouteSegment.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingPriceSystem
+{
+    internal class RouteSegment
+    {
+
+        internal String PointA { get; set; }
+        internal String PointB { get; set; }
+        internal String Vehicle { get; set; }
+    }
+}
