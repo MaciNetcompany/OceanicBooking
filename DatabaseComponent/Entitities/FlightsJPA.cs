e1319588808f48532cb59cd3965aae26811cddd4
Maciej Szewczyk <maci@netcompany.com>
2023-01-13 11:42:23 +0100

    csv export now properly use data from db

--- DatabaseComponent/Entitities/FlightsJPA.cs
+++ DatabaseComponent/Entitities/FlightsJPA.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,7 @@ namespace DatabaseComponent.Entitities
     public class FlightsJPA
     {
         public Guid ID { get; set; }
-        public BookingsJPA order_booking_id { get; set; }
+        public Guid order_booking_id { get; set; }
         public string origin { get; set; }
         public string destination { get; set; }
         public int price { get; set; }
