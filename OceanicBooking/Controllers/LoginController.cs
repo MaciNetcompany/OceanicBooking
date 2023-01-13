49d0c7225b411adc9eb7ea38a60aaca7dd87cc63
Maciej Szewczyk <maci@netcompany.com>
2023-01-13 04:28:56 +0100

    mock login

--- OceanicBooking/Controllers/LoginController.cs
+++ OceanicBooking/Controllers/LoginController.cs
@@ -6,24 +6,35 @@ namespace OceanicBooking.Controllers
     [ApiController]
     public class LoginController : Controller
     {
-
+        string adminUserName="admin";
+        private string bookerUserName = "booker";
 
         public ActionResult Index()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> OnPost()
         {
             var d = 2;
             var username = Request.Form["username"];
             var password = Request.Form["password"];
-            if (username[0]!=null&& password[0]!=null)
+            if (username[0] == null || password[0] == null)
             {
-                d = 3;
+                return BadRequest();
             }
 
-            return RedirectToAction("Index", "Home");
+            if (username[0].Equals(adminUserName))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else if (username[0].Equals(bookerUserName))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return BadRequest();
         }
 
         [Route("auth")]
