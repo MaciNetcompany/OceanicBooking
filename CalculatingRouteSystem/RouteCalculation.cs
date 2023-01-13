c7b4e9605e26e227c57a51e03423ca5e0ffce45d
Aleksander Øster Nielsen <aon@netcompany.com>
2023-01-13 11:15:57 +0100

    Route calculation is now possible, usen adjacency matrix buil from cities, also need

---
+++ CalculatingRouteSystem/RouteCalculation.cs
@@ -0,0 +1,74 @@
+﻿namespace CalculatingRouteSystem
+{
+    public class RouteCalculation
+    {
+
+        private int minDistance(int[] dist,
+                        bool[] sptSet, int graphSize)
+        {
+            int min = int.MaxValue, min_index = -1;
+
+            for (int v = 0; v < graphSize; v++)
+                if (sptSet[v] == false && dist[v] <= min)
+                {
+                    min = dist[v];
+                    min_index = v;
+                }
+
+            return min_index;
+        }
+
+
+        private int dijkstra(int[,] graph, int src, int dest)
+        {
+            int[] dist = new int[graph.GetLength(1)];
+
+            bool[] sptSet = new bool[graph.GetLength(1)];
+
+            for (int i = 0; i < graph.GetLength(1); i++)
+            {
+                dist[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
+
+            dist[src] = 0;
+
+            for (int count = 0; count < graph.GetLength(1) - 1; count++)
+            {
+
+                int u = minDistance(dist, sptSet, graph.GetLength(1));
+
+                sptSet[u] = true;
+
+                for (int v = 0; v < graph.GetLength(1); v++)
+
+
+                    if (!sptSet[v] && graph[u, v] != 0 &&
+                         dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
+                        dist[v] = dist[u] + graph[u, v];
+            }
+
+            return dist[dest];
+        }
+
+        private int calculateTime(int segCount)
+        {
+            return segCount * 8;
+        }
+
+        private int calculatePrice(int segCount, int parcelPrice)
+        {
+            return segCount * parcelPrice;
+        }
+
+        public int shortestPathTime(int src, int dest, int[,] graph)
+        {
+            return calculateTime(dijkstra(graph, src, dest));
+        }
+
+        public int shortestPathPrice(int src, int dest, int[,] graph, int parcelPrice)
+        {
+            return calculatePrice(dijkstra(graph, src, dest), parcelPrice);
+        }
+    }
+}
---
+++ CalculatingRouteSystemTests1/RouteCalculationTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatingRouteSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingRouteSystem.Tests
+{
+    [TestClass()]
+    public class RouteCalculationTests
+    {
+        [TestMethod()]
+        public void shortestPathTimeTest()
+        {
+            int src = 0;
+            int dest = 5;
+            int[,] graph = new int[,] {
+                {0, 1, 1, 0, 0, 0},
+                {1, 0, 1, 1, 0, 0},
+                {1, 1, 0, 0, 1, 0},
+                {0, 1, 0, 0, 1, 1},
+                {0, 0, 1, 1, 0, 1},
+                {0, 0, 0, 1, 1, 0}
+            };
+
+            RouteCalculation routing = new RouteCalculation();
+            int time = routing.shortestPathTime(src, dest, graph);
+
+            Assert.AreEqual(24, time);
+        }
+
+        [TestMethod()]
+        public void shortestPathPriceTest()
+        {
+            int src = 0;
+            int dest = 5;
+            int[,] graph = new int[,] {
+                {0, 1, 1, 0, 0, 0},
+                {1, 0, 1, 1, 0, 0},
+                {1, 1, 0, 0, 1, 0},
+                {0, 1, 0, 0, 1, 1},
+                {0, 0, 1, 1, 0, 1},
+                {0, 0, 0, 1, 1, 0}
+            };
+
+            int parcelPrice = 40;
+
+            RouteCalculation routing = new RouteCalculation();
+            int price = routing.shortestPathPrice(src, dest, graph, parcelPrice);
+
+            Assert.AreEqual(120, price);
+        }
+    }
+}
